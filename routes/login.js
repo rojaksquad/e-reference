fea3d1858f72c75da2a74f550029987920652363
Pownyfals <pownyfals@gmail.com>
2023-05-08 15:14:00 +0700

    fix UI for upload references

--- routes/login.js
+++ routes/login.js
@@ -25,6 +25,7 @@ router.post("/", (req, res) => {
       if (!err) {
         if (docs.length > 0) {
           bcrypt.compare(password, docs[0].password, function (err, result) {
+            console.log(password, docs[0].password );
             if (err) {
               console.log(err);
               res.json({
@@ -34,12 +35,13 @@ router.post("/", (req, res) => {
               if (result) {
                 res.redirect("/upload");
               } else {
+                console.log("password did not match", result);
                 res.redirect("/login");
               }
             }
           });
         } else {
-          res.redirect("/register");
+          res.redirect("/login");
         }
       } else {
         console.error(err);
